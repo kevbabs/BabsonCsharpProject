64346f77f96bad1dd065db1788d985d3bb532a4b
kevin Babs <kevbabs@hotmail.com>
2018-07-28 23:22:14 +0100

    Finished swit & control statement

--- PratiseCode/switchStatement/Program.cs
+++ PratiseCode/switchStatement/Program.cs
@@ -10,27 +10,51 @@ namespace switchStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Borough name");
-            string boroughName = Console.ReadLine();
+            //Console.WriteLine("Enter your age");
 
-            //switch statement
-            switch (boroughName)
+            //console.WriteLine("Enter your age");
+            ////string boroughName = Console.ReadLine();
+            //int myAge = Convert.ToInt32(Console.ReadLine());
+
+            ////switch statement
+            //switch (myAge)
+            //{
+            //    case 30:
+            //        Console.WriteLine(" My age is {0}", myAge);
+            //        break;
+            //    case 45:
+            //        Console.WriteLine("My age is {0}", myAge);
+            //        break;
+            //    default:
+            //        Console.WriteLine("Sorry you are not in the age group, {0}, try another age group", myAge);
+            //        break;
+
+            Console.WriteLine("Did statement run");
+
+            for (int counter = 0; counter <= 20; counter += 5)
             {
-                case "sutton":
-                    Console.WriteLine(" I live in {0}", boroughName);
-                    break;
-                case "newham":
-                    Console.WriteLine("I live in {0}" + boroughName);
-                    break;
-                default:
-                    Console.WriteLine("Sorry the borough is not know, {0}, is not listed", boroughName);
-                    break;
-
-              
+                Console.WriteLine("Display the counter" + counter);
             }
+            Console.WriteLine();
 
-            Console.ReadKey();
+            controll statement
+
+            string[] londonBorough = { "newham", "sutton", "barnet", "earling" };
+            foreach (var borough in londonBorough)
+            {
+                if (borough.ToLower().Equals("SUTTON".ToLower()))
+                {
+                    Console.WriteLine("I found my borough {0}", borough.ToLower());
+                }
+                else
+                {
+                    Console.WriteLine("borough {0} this not my borough", borough);
+                }
+
+                Console.ReadLine();
+            }
 
         }
     }
-}
+
+
