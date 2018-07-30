0207ef01d8108ce0179ef8f94aecc790cd857622
kevin Babs <kevbabs@hotmail.com>
2018-07-30 22:59:30 +0100

    Finish control loop

--- PratiseCode/switchStatement/Program.cs
+++ PratiseCode/switchStatement/Program.cs
@@ -10,46 +10,29 @@ namespace switchStatement
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Enter your age");
 
-            //console.WriteLine("Enter your age");
-            ////string boroughName = Console.ReadLine();
-            //int myAge = Convert.ToInt32(Console.ReadLine());
+            //do while loop
+            Console.WriteLine("Enter the kid temperature");
+            int kidTemp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The kids temp is {0}", kidTemp);
 
-            ////switch statement
-            //switch (myAge)
+            //    do
+            //    {
+            //        Console.WriteLine("Give the patient 100g of paracetamore");
+            //        kidTemp++;
+            //    } while (kidTemp <35);
+            //Console.WriteLine("Don't give medication if tem is greater than 35");
+
+            //while (kidTemp <15)
             //{
-            //    case 30:
-            //        Console.WriteLine(" My age is {0}", myAge);
-            //        break;
-            //    case 45:
-            //        Console.WriteLine("My age is {0}", myAge);
-            //        break;
-            //    default:
-            //        Console.WriteLine("Sorry you are not in the age group, {0}, try another age group", myAge);
-            //        break;
-
-            Console.WriteLine("Did statement run");
-
-            for (int counter = 0; counter <= 20; counter += 5)
+            //    Console.WriteLine("Give the kids 100g of paracetemore");
+            //    kidTemp++;
+            //    Console.WriteLine(" the kids tem is now {0}", kidTemp);
+            //}
+            for (int drugDosage = kidTemp; drugDosage < 40; drugDosage++)
             {
-                Console.WriteLine("Display the counter" + counter);
+                Console.WriteLine("Increase the doseage to {0}", drugDosage);
             }
-            Console.WriteLine();
-
-            controll statement
-
-            string[] londonBorough = { "newham", "sutton", "barnet", "earling" };
-            foreach (var borough in londonBorough)
-            {
-                if (borough.ToLower().Equals("SUTTON".ToLower()))
-                {
-                    Console.WriteLine("I found my borough {0}", borough.ToLower());
-                }
-                else
-                {
-                    Console.WriteLine("borough {0} this not my borough", borough);
-                }
 
                 Console.ReadLine();
             }
