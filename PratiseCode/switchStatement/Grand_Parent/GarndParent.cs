bae76f1ce0e89b27a8e5641345db5c046e45315d
kevin Babs <kevbabs@hotmail.com>
2018-08-01 22:36:16 +0100

    Access modifieer continue

---
+++ PratiseCode/switchStatement/Grand_Parent/GarndParent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchStatement.FamilyTree
+{
+    public class GarndParent
+    {
+        public string ChiefTitle = "Sariki";
+
+        public void CallChiefTitle()
+        {
+            Console.WriteLine("My title is {0}", ChiefTitle);
+        }
+
+    }
+}
---
+++ PratiseCode/switchStatement/Parent/DadMum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchStatement.Parent
+{
+    public class DadMum
+    {
+        protected string ParentName = "Babs";
+
+        public void CallParent()
+        {
+            string newName;
+            Console.WriteLine("My parent surname is {0}", ParentName);
+           
+        }
+
+        protected void NewName()
+        {
+          Console.WriteLine("My name is  still {0}", ParentName);
+         
+        }
+    }
+}
