953bafdddc31194e20f6b814cec8de8d7996dfa2
kevin Babs <kevbabs@hotmail.com>
2018-08-01 19:54:59 +0100

    Acces modifier

--- PratiseCode/switchStatement/Program.cs
+++ PratiseCode/switchStatement/Program.cs
@@ -6,38 +6,27 @@ using System.Threading.Tasks;
 
 namespace switchStatement
 {
-    class Program
+    public class Program 
     {
         static void Main(string[] args)
         {
+            publicMethod pm = new publicMethod();
+            pm.age = 20;
+            pm.name = "Davina";
+            pm.StudentEnroll();
 
-            //do while loop
-            Console.WriteLine("Enter the kid temperature");
-            int kidTemp = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The kids temp is {0}", kidTemp);
 
-            //    do
-            //    {
-            //        Console.WriteLine("Give the patient 100g of paracetamore");
-            //        kidTemp++;
-            //    } while (kidTemp <35);
-            //Console.WriteLine("Don't give medication if tem is greater than 35");
 
-            //while (kidTemp <15)
-            //{
-            //    Console.WriteLine("Give the kids 100g of paracetemore");
-            //    kidTemp++;
-            //    Console.WriteLine(" the kids tem is now {0}", kidTemp);
-            //}
-            for (int drugDosage = kidTemp; drugDosage < 40; drugDosage++)
-            {
-                Console.WriteLine("Increase the doseage to {0}", drugDosage);
-            }
 
-                Console.ReadLine();
-            }
+            PrivateMeth prim = new PrivateMeth();
+            prim.ChildrenEnroll(20, "Bisi");
+            prim.
+            
 
+            Console.ReadKey();
         }
-    }
+        
+     }
+}
 
 
---
+++ PratiseCode/switchStatement/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchStatement
+{
+    public class Triangle
+    {
+        // triangle variable
+        public int triHeigh;
+        public int triBase;
+        public int areaAnswer;
+
+        // Area method
+        public void TriangleArea()
+        {
+            Console.WriteLine();
+            areaAnswer = triHeigh*triBase;
+            Console.WriteLine("The area of this triangle is {0}", areaAnswer);
+
+        }
+
+        public int Pentangon()
+        {
+            Console.WriteLine();
+            areaAnswer = (triBase * triHeigh) * 1/2;
+            Console.WriteLine("The area of this pentangon is {0}", areaAnswer);
+            return areaAnswer;
+        }
+
+        // This my third method that take arguement
+        public void Octagon(int paramOne, int paramTwo)
+        {
+            Console.WriteLine();
+            triHeigh = paramOne;
+            triBase = paramTwo;
+            areaAnswer = (paramOne + paramTwo)*2;
+            Console.WriteLine("The area of this Octagon is {0}", areaAnswer);
+        }
+    }
+}
