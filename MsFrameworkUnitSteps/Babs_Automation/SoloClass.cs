fed584ec602f3eb3b46c34be7e6de0d322e145a4
kevin Babs <kevbabs@hotmail.com>
2018-09-06 14:57:59 +0100

    Create First unit test

---
+++ MsFrameworkUnitSteps/Babs_Automation/SoloClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Babs_Automation
+{
+    [TestClass]
+
+    public class SoloClass
+    {
+        [TestMethod]
+        public void SoloMethodFromAnotherClass()
+        {
+            Console.WriteLine("I am from Solo class");
+        }
+    }
+}
