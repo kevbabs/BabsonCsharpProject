a2ec4790c5fcaaa572b2863e6e1b1acec7ce8e19
kevin Babs <kevbabs@hotmail.com>
2018-11-10 21:36:00 +0000

    Create RadiobuttonHelper class

---
+++ MsFrameworkUnitSteps/Babs_Automation/Component_Helper/ButtonHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace Babs_Automation.Component_Helper
+{
+    public class ButtonHelper
+    {
+        private static IWebElement _element;
+
+        public static bool IsButtonEnable(By locator)
+        {
+            _element = GenericHelper.GetElement(locator);
+            return _element.Enabled ;
+        }
+        public static void ClickButton(By locator)
+        {
+            _element = GenericHelper.GetElement(locator);
+            _element.Click();
+        }
+    }
+}
