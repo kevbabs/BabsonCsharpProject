f8850ea17a03c2347195f0104cbc25baa25d359a
kevin Babs <kevbabs@hotmail.com>
2018-11-11 22:11:27 +0000

    Create WaitHelper

---
+++ MsFrameworkUnitSteps/Babs_Automation/Component_Helper/Waithelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Babs_Automation.Setting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Events;
+using OpenQA.Selenium.Support.UI;
+
+namespace Babs_Automation.Component_Helper
+{
+    public class WaitHelper
+    {
+        private static IWebElement _element;
+
+        public static void WaitForElement(By locator)
+        {
+            var  wait = new WebDriverWait(ObjectRepository.Driver, TimeSpan.FromMilliseconds(15));
+            // Lambda expression
+            _element = wait.Until(d => d.FindElement(locator));
+        }
+
+    }
+}
