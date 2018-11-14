9bace454859292b54d1ce5f55f46fe692f0c52ff
kevin Babs <kevbabs@hotmail.com>
2018-11-14 22:22:11 +0000

    Create DropdownHelper class

---
+++ MsFrameworkUnitSteps/Babs_Automation/Component_Helper/DropDownHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Babs_Automation.Component_Helper
+{
+    public class DropDownHelper
+    {
+        private static SelectElement _select;
+
+        public static void SelectElementByIndex(By locator, int indextValue)
+        {
+            _select = new SelectElement(GenericHelper.GetElement(locator));
+            _select.SelectByIndex(indextValue);
+        }
+
+        public static void SelectElementByText(By locator, string visibleText)
+        {
+            _select = new SelectElement(GenericHelper.GetElement(locator));
+            _select.SelectByText(visibleText);
+        }
+
+        public static void SelectElementByValue(By locator, string visibleText)
+        {
+            _select = new SelectElement(GenericHelper.GetElement(locator));
+            _select.SelectByValue(visibleText);
+        }
+
+        public static IList<string> AllDropdownMenuItem(By locator)
+        {
+            _select = new SelectElement(GenericHelper.GetElement(locator));
+            return _select.Options.Select(x => x.Text).ToList();
+        }
+
+    }
+}
